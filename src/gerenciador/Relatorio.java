70f5de2bd8a442747d6660952d58bb97e5f8f396
patrikoliver <oliveirapimentel0503@gmail.com>
2024-06-29 20:40:35 -0300

    adicionado relatorio de entrada e saida de veiculos

--- src/gerenciador/Relatorio.java
+++ src/gerenciador/Relatorio.java
@@ -24,21 +24,61 @@ public class Relatorio {
     }
 
     public void gerarRelatorioOcupacao() {
-        System.out.println("Relatório de Ocupação:");
+    	
+    	StringBuilder relatorio = new StringBuilder();
+    	
+    	System.out.println("Relatório de Ocupação:");
+    	relatorio.append("Relatório de Ocupação: \n");
+    	
         for (EntradaSaida es : registros) {
             String status = es.getVagas().getStatus() == 1 ? "Livre" : es.getVagas().getStatus() == 0 ? "Ocupado" : "Reservado";
             System.out.println("Veículo: " + es.getVeiculo().getDetalhes() + ", Vaga: " + es.getVagas().getNumero() + ", Status: " + status);
+            relatorio.append("Veículo: " + es.getVeiculo().getDetalhes() + ", Vaga: " + es.getVagas().getNumero() + ", Status: " + status+"\n");
+            
         }
+        //return relatorio.toString();
     }
 
     public void gerarRelatorioFinanceiro() {
         double total = 0;
+        StringBuilder relatorio = new StringBuilder();
         System.out.println("Relatório Financeiro Diário:");
+        relatorio.append("Relatório Financeiro Diário: \n");
+        
         for (Pagamento pagamento : pagamentos) {
             total += pagamento.getValor();
         }
         System.out.println("Total arrecadado: R$ " + total);
+        relatorio.append("Total arrecadado: R$ " + total).append("\n");
+        //return relatorio.toString();
     }
+    
+    public String relatorioEntradaSaida() {
+    	
+    	StringBuilder relatorio = new StringBuilder();
+    	StringBuilder relatorioEntrada = new StringBuilder();
+    	StringBuilder relatorioSaida = new StringBuilder();
+    	
+    	if(registros.isEmpty()) {
+    		return "Nao ha nenhum registro de entrada ou saida";
+    	}
+		for(EntradaSaida registro: registros) {
+			if(registro.getHorarioSaida() == 0) {
+				relatorioEntrada.append("Veiculo : ").append(registro.getVeiculo().getTipo()).append("\n");
+			}else {
+				relatorioSaida.append("Veiculo : ").append(registro.getVeiculo().getTipo()).append("\n");
+			}
+		}
+		if(!relatorioEntrada.isEmpty()) {
+			relatorio.append("Veículos que estão no estacionamento \n:");
+			relatorio.append(relatorioEntrada);
+		}
+		if(!relatorioSaida.isEmpty()) {
+			relatorio.append("Veículos que sairam do estacionamento \n:");
+			relatorio.append(relatorioSaida);
+		}
+		return relatorio.toString();
+	}
 
     
 }
